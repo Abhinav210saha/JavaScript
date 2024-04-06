9d42935f594fc6ccc8febe1a88560d75a18f7804
Abhinav210saha <156905980+Abhinav210saha@users.noreply.github.com>
2024-04-06 13:41:34 +0000

    conatiner config done

---
+++ 02_basics/01_array.js
@@ -0,0 +1,30 @@
+//  const myArray=[1,2,3,4,5]
+
+//  myArray.push(6)
+//  myArray.pop()
+
+
+//  console.log(myArray);
+
+const marvel_hero=["Iron", "Spider", "Thor"]
+const dc_hero=["superHero", "flash", "batman"]
+
+//marvel_hero.push(dc_hero)
+// console.log(marvel_hero);
+
+// const allHero= marvel_hero.concat(dc_hero)
+// console.log(allHero);
+
+// const all_Hero=[...marvel_hero,...dc_hero]
+// console.log(all_Hero);
+
+let score1=377
+let score2=765
+let score3=899
+
+// console.log(Array.of(score1,score2,score3))
+
+const randomArray=[1,2,3,4,[4.5,6],6,7,8,[56]]
+const randomArr= randomArray.flat(Infinity)
+// console.log(randomArr);
+
